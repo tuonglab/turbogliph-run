7e8d848d40c1090264ab2627e414112f33c26ba0
amoschoomy <amoschoouqconnect@gmail.com>
2023-08-03 10:36:26 +1000

    WIP(TBT): allow user to specify directory of files to analyse

--- run_turbogliph.r
+++ run_turbogliph.r
@@ -2,11 +2,19 @@
 
 library("turboGliph")
 library("parallel")
-# TODO: specify directory option
-setwd("/scratch/user/uqachoo1/phs002504-trust4")
+
+# Get the command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+
+# Check if a directory was specified
+if (length(args) == 0) {
+    stop("Please specify a directory to run turbogliph on")
+}
+
+# Set the working directory to the specified directory
+setwd(args[1])
 
 files <- list.files(pattern = "airr_extracted\\.tsv", recursive = TRUE)
-
 
 # Define a function to read a single file
 read_file <- function(file) {
@@ -22,9 +30,8 @@ cl <- makeCluster(8)
 
 clusterExport(cl, "list_of_data_frames")
 
-
 # Define a function to process a single data frame
-process_data_frame <- function(df) {
+process_data_frame <- function(df, output_dir, file_name) {
     # Process the data frame
     # Rename the columns of the data frame
     colnames(df)[colnames(df) == "junction_aa"] <- "CDR3b"
@@ -33,16 +40,12 @@ process_data_frame <- function(df) {
 
     # TODO: run gliph2 here
     # TODO: specify output directory for result files
-    print(df)
+    output_dir <- file.path(output_dir, file_name)
+    gliph2(df, n_cores = NULL, global_vgene = TRUE, result_folder = output_dir, motif_length = c(3, 4, 5, 6), min_seq_length = 8)
 }
 
 # Apply the function to each data frame in parallel
-parLapply(cl, list_of_data_frames, process_data_frame)
+parLapply(cl, list_of_data_frames, process_data_frame, args[1])
 
 # Stop the cluster
 stopCluster(cl)
-
-
-
-
-# gliph2(df, n_cores = NULL, global_vgene = TRUE, result_folder = "/home/uqachoo1/gliph2output", motif_length = c(3, 4, 5, 6), min_seq_length = 8)
