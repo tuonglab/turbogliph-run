47eac0766416b1039558d04d916b9c5fdbfad94c
amoschoomy <amoschoouqconnect@gmail.com>
2024-02-07 10:21:52 +1000

    fix and catch turbogliph errors for lack of info

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=64
-#SBATCH --mem=32G
+#SBATCH --mem=75G
 #SBATCH --job-name=turbogliph
-#SBATCH --time=2:00:00
+#SBATCH --time=100:00:00
 #SBATCH --partition=general
 #SBATCH --account=a_kelvin_tuong
-#SBATCH -o run.output
-#SBATCH -e run.error
+#SBATCH -o run_PHS002529.output
+#SBATCH -e run_PHS002529.error
 
-srun /home/uqachoo1/mambaforge/envs/R/bin/Rscript run_turbogliph.r /scratch/project/tcr_ml/TRUST4output_v2/PHS002504 /scratch/project/tcr_ml/GLIPH2_output/PHS002504
+srun /home/uqachoo1/mambaforge/envs/R/bin/Rscript run_turbogliph.r /scratch/project/tcr_ml/TRUST4output_v2/PHS002529 /scratch/project/tcr_ml/GLIPH2_output/PHS002529
--- run_turbogliph.r
+++ run_turbogliph.r
@@ -1,7 +1,6 @@
 #!/usr/bin/env Rscript
 
 library("turboGliph")
-library("parallel")
 
 # Get the command line arguments
 args <- commandArgs(trailingOnly = TRUE)
@@ -32,19 +31,25 @@ read_file <- function(file) {
     }
 }
 
-
 list_of_data_frames <- lapply(files, read_file)
 list_of_data_frames <- Filter(Negate(is.null), list_of_data_frames)
 
-cl <- makeCluster(8)
-
-clusterExport(cl, "list_of_data_frames")
-clusterExport(cl, "gliph2")
-
 process_data_frame <- function(data, output_dir) {
+    # Check if data is NULL
+    if (is.null(data)) {
+        print("Received NULL data")
+        return(NULL)
+    }
+
     # Extract the data frame and file name from the input list
     df <- data$df
     file_name <- data$file_name
+
+    # Check if df or file_name is NULL
+    if (is.null(df) || is.null(file_name)) {
+        print("Received NULL df or file_name")
+        return(NULL)
+    }
 
     # Process the data frame
     # Rename the columns of the data frame
@@ -53,11 +58,43 @@ process_data_frame <- function(data, output_dir) {
     colnames(df)[colnames(df) == "j_call"] <- "TRBJ"
 
     output_dir <- file.path(output_dir, file_name)
-    gliph2(df, n_cores = NULL, global_vgene = TRUE, result_folder = output_dir, motif_length = c(3, 4, 5), min_seq_length = 8)
+
+    # Check if the number of rows in the data frame is less than 2
+    if (nrow(df) < 2) {
+        print(paste("Skipping gliph2 for file", file_name, "as it has less than 2 rows"))
+        return(NULL)
+    }
+
+    tryCatch(
+        {
+            # Try running gliph2 with the original parameters
+            gliph2(df, n_cores = NULL, global_vgene = TRUE, result_folder = output_dir, motif_length = c(3, 4, 5), min_seq_length = 8)
+        },
+        error = function(e) {
+            # If an error occurs, print the error message
+            print(paste("Caught an error with file", file_name, ":", e$message))
+
+            # Save the error message to a log file
+            write(paste("Caught an error with file", file_name, ":", e$message), file = "error_log.txt", append = TRUE)
+
+            # Then try running gliph2 with the alternative parameters
+            tryCatch(
+                {
+                    gliph2(df, n_cores = NULL, global_vgene = TRUE, result_folder = output_dir, motif_length = c(3, 4, 5), min_seq_length = 8, local_similarities = FALSE) # Change the parameters as needed
+                },
+                error = function(e) {
+                    # If an error occurs, print the error message
+                    print(paste("Caught an error with file", file_name, "on the second attempt:", e$message))
+
+                    # Save the error message to a log file
+                    write(paste("Caught an error with file", file_name, "on the second attempt:", e$message), file = "error_log.txt", append = TRUE)
+                }
+            )
+        }
+    )
 }
 
-# Apply the function to each data frame in parallel
-parLapply(cl, list_of_data_frames, process_data_frame, output_dir)
 
-# Stop the cluster
-stopCluster(cl)
+
+# Apply the function to each data frame
+lapply(list_of_data_frames, process_data_frame, output_dir)
